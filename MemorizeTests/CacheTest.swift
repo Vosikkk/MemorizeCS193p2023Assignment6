b287b97e30a3a7d654d581aa868e7e565c8898c0
Vosik <vosikvos@gmail.com>
2024-03-28 01:54:40 +0200

    F - add test-target and first fail test

---
+++ MemorizeTests/CacheTest.swift
@@ -0,0 +1,21 @@
+//
+//  CacheTest.swift
+//  MemorizeTests
+//
+//  Created by Саша Восколович on 28.03.2024.
+//
+
+import XCTest
+@testable import Memorize
+
+final class CacheTest: XCTestCase {
+
+   
+    
+    func test() {
+        XCTFail()
+    }
+    
+    
+
+}
